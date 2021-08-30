6ce6703438996b180c663feed1953d2773ea856b
Blake <krypt0mux@gmail.com>
2021-08-30 18:20:25 +1000

    Updates

--- tprox/traversal/traversal.go
+++ tprox/traversal/traversal.go
@@ -72,7 +72,7 @@ func TestTraversal(wg *sync.WaitGroup, url string, payload string, silent bool) 
 			blue := color.New(color.FgBlue, color.Bold).SprintFunc()
 			white := color.New(color.FgWhite, color.Bold).SprintFunc()
 			green := color.New(color.FgGreen, color.Bold).SprintFunc()
-			gologger.Info().Msg("%s%s%s %s ", white("["), blue("Proxy"), white("]"), white("Proxy, Bruteforcing..."))
+			fmt.Printf("%s%s%s %s ", white("["), blue("Proxy"), white("]"), white("Proxy, Bruteforcing..."))
 
 			// Start bruteforcing for files and directories
 			words := make(chan string)
