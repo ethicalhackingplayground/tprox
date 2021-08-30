6aefa2a42e49323b24978427b422b75cd7c7412d
Blake <krypt0mux@gmail.com>
2021-08-30 23:36:26 +1000

    Updates

--- tprox/main.go
+++ tprox/main.go
@@ -19,7 +19,7 @@ var Payloads = [3]string{"..%2f", "..;/", "%2e%2e%2f"}
 func main() {
 	parsed, crawl, silent := args.ParseArgs()
 	if parsed {
-		gologger.Debug().Msg("Finding misconfigured proxies\n)
+		gologger.Debug().Msg("Finding misconfigured proxies\n")
 
 		run(crawl, silent)
 	}
