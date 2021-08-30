7256937155bd2dbac7e481cf6d64e6ee367ef863
Blake <krypt0mux@gmail.com>
2021-08-30 18:27:44 +1000

    Updates

---
+++ tprox/vendor/github.com/ethicalhackingplayground/tprox/src/discover/discover.go
@@ -0,0 +1,59 @@
+package discover
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"sync"
+
+	"github.com/ethicalhackingplayground/tprox/src/args"
+	"github.com/fatih/color"
+	"github.com/projectdiscovery/gologger"
+)
+
+// Start the content discovery for files and directories
+func BruteForDirAndFile(client http.Client, wg *sync.WaitGroup, url string, testUrl string, word string, silent bool) {
+
+	info := color.New(color.FgWhite, color.Bold).SprintFunc()
+	white := color.New(color.FgWhite, color.Bold).SprintFunc()
+	green := color.New(color.FgGreen, color.Bold).SprintFunc()
+
+	contentFound := testUrl + word
+	contentNotFound := url + "/" + word
+	resp1, err := http.Get(contentFound)
+	if err != nil {
+		return
+	}
+
+	if silent == false {
+		gologger.Debug().Msg(contentFound)
+	}
+
+	resp2, err := http.Get(contentNotFound)
+	if err != nil {
+		return
+	}
+	if resp1.StatusCode == 200 {
+		if resp2.StatusCode == 404 || resp2.StatusCode == 403 || resp2.StatusCode == 401 || resp2.StatusCode == 400 {
+			if args.Output != "" {
+
+				f, err := os.Create(args.Output)
+				if err != nil {
+					return
+				}
+				defer f.Close()
+
+				_, err2 := f.WriteString(contentFound + "\n")
+
+				if err2 != nil {
+					return
+				}
+			}
+
+			fmt.Println("")
+			fmt.Printf("%s%s%s %s\n", white("["), green("FOUND"), white("]"), info(contentFound))
+		}
+
+	}
+
+}
---
+++ tprox/vendor/github.com/ethicalhackingplayground/tprox/tprox/traversal/traversal.go
@@ -0,0 +1,107 @@
+package traversal
+
+import (
+	"bufio"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+
+	"github.com/ethicalhackingplayground/tprox/src/args"
+	"github.com/ethicalhackingplayground/tprox/src/discover"
+	"github.com/projectdiscovery/gologger"
+)
+
+// Craft the new url to test, this works as the following.
+// 1.) Input the Url to test
+// 2.) Calculate the number of paths
+// 3.) Append the payload the same number of times as the paths
+func CraftTestUrl(count int, url string, payload string) string {
+	var traversal = ""
+	for i := 0; i < count; i++ {
+		traversal = traversal + payload
+	}
+
+	return url + "/" + traversal + "/"
+
+}
+
+// Test for proxy traversal attacks
+func TestTraversal(wg *sync.WaitGroup, url string, payload string, silent bool) {
+
+	client := http.Client{}
+
+	// Get the test url
+	paths := strings.Split(url, "/")
+	pathCount := len(paths) - 2
+	testUrl := CraftTestUrl(pathCount, url, payload)
+
+	// Get the response from the server to
+	// Perform ongoing testing for proxy misconfigs
+	req, err := http.NewRequest("GET", testUrl, nil)
+	if err != nil {
+		return
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return
+	}
+
+	// Check if the page is a 404, if it is we may be in the root api
+	// From here onwards we need to perform directory content discovery to
+	// Find internal files or endpoints
+	if resp.StatusCode == 400 {
+
+		// Get the test url
+		paths := strings.Split(url, "/")
+		pathCount := len(paths) - 3
+		testUrl := CraftTestUrl(pathCount, url, payload)
+
+		// Get the response from the server to
+		// Perform ongoing testing for proxy misconfigs
+		req, err := http.NewRequest("GET", testUrl, nil)
+		if err != nil {
+			return
+		}
+		resp, err := client.Do(req)
+		if err != nil {
+			return
+		}
+
+		if resp.StatusCode == 404 {
+			blue := color.New(color.FgBlue, color.Bold).SprintFunc()
+			white := color.New(color.FgWhite, color.Bold).SprintFunc()
+			green := color.New(color.FgGreen, color.Bold).SprintFunc()
+			gologger.Info().Msg("%s%s%s %s ", white("["), blue("Proxy"), white("]"), white("Proxy, Bruteforcing..."))
+
+			// Start bruteforcing for files and directories
+			words := make(chan string)
+
+			for i := 0; i < args.Threads; i++ {
+				wg.Add(1)
+				go func() {
+
+					// wordlist brute channel loop
+					for word := range words {
+						discover.BruteForDirAndFile(client, wg, url, testUrl, word, silent)
+					}
+					wg.Done()
+				}()
+
+			}
+
+			// Read in the wordlist list
+			wordFile, err := os.Open(args.Wordlist)
+			if err != nil {
+				return
+			}
+
+			wordBytes := bufio.NewScanner(wordFile)
+			for wordBytes.Scan() {
+				words <- wordBytes.Text()
+			}
+			close(words)
+
+		}
+	}
+}
