29ffcf0f3886e4469926105068f484394843b023
zoidex <blake@pentesterlab.com>
2021-09-16 23:06:06 +1000

    Bug fix

--- tprox/discover/discover.go
+++ tprox/discover/discover.go
@@ -14,71 +14,7 @@ import (
 // Start the content discovery for files and directories
 func BruteForDirAndFile(client http.Client, wg *sync.WaitGroup, url string, testUrl string, word string, silent bool, traverse bool, discover bool, check bool) {
 	color.NoColor = false
-
-	if word == "" {
-		info := color.New(color.FgWhite, color.Bold).SprintFunc()
-		white := color.New(color.FgWhite, color.Bold).SprintFunc()
-		green := color.New(color.FgGreen, color.Bold).SprintFunc()
-
-		fullUrl := url
-		endPathLength := len(strings.Split(fullUrl, "/"))
-		endPath := strings.Split(fullUrl, "/")[endPathLength-1]
-		rootDomain := strings.Split(fullUrl, "/")[0] + "//" + strings.Split(fullUrl, "/")[2] + "/" + endPath
-
-		contentFound := testUrl + word
-		resp1, err := http.Get(contentFound)
-		if err != nil {
-			return
-		}
-
-		resp2, err := http.Get(rootDomain)
-		if err != nil {
-			return
-		}
-
-		if traverse {
-			if resp1.StatusCode == 200 && resp2.StatusCode != 200 {
-				if args.Output != "" {
-
-					f, err := os.Create(args.Output)
-					if err != nil {
-						return
-					}
-					defer f.Close()
-
-					_, err2 := f.WriteString(contentFound + "\n")
-
-					if err2 != nil {
-						return
-					}
-				}
-				fmt.Printf("\n\n%s%s%s %s\n\n", white("["), green("FOUND"), white("]"), info(contentFound))
-				defer color.Unset() // Use it in your function
-
-			}
-		} else {
-
-			if resp1.StatusCode == 200 {
-				if args.Output != "" {
-
-					f, err := os.Create(args.Output)
-					if err != nil {
-						return
-					}
-					defer f.Close()
-
-					_, err2 := f.WriteString(contentFound + "\n")
-
-					if err2 != nil {
-						return
-					}
-				}
-
-				fmt.Printf("\n\n%s%s%s %s\n\n", white("["), green("FOUND"), white("]"), info(contentFound))
-				defer color.Unset() // Use it in your function
-			}
-		}
-	} else {
+	if discover {
 		info := color.New(color.FgWhite, color.Bold).SprintFunc()
 		white := color.New(color.FgWhite, color.Bold).SprintFunc()
 		green := color.New(color.FgGreen, color.Bold).SprintFunc()
@@ -90,54 +26,153 @@ func BruteForDirAndFile(client http.Client, wg *sync.WaitGroup, url string, test
 		if err != nil {
 			return
 		}
-
 		resp2, err := http.Get(rootDomain)
 		if err != nil {
 			return
 		}
+		if resp1.StatusCode == 200 && resp2.StatusCode != 200 {
+			if args.Output != "" {
 
-		if traverse {
-			if resp1.StatusCode == 200 && resp2.StatusCode != 200 {
-				if args.Output != "" {
-
-					f, err := os.Create(args.Output)
-					if err != nil {
-						return
-					}
-					defer f.Close()
-
-					_, err2 := f.WriteString(contentFound + "\n")
-
-					if err2 != nil {
-						return
-					}
+				f, err := os.Create(args.Output)
+				if err != nil {
+					return
 				}
-				fmt.Printf("\n\n%s%s%s %s\n\n", white("["), green("FOUND"), white("]"), info(contentFound))
-				defer color.Unset() // Use it in your function
+				defer f.Close()
 
+				_, err2 := f.WriteString(contentFound + "\n")
+
+				if err2 != nil {
+					return
+				}
+			}
+			fmt.Printf("\n\n%s%s%s %s\n\n", white("["), green("FOUND"), white("]"), info(contentFound))
+			defer color.Unset() // Use it in your function
+
+		}
+	} else {
+		if word == "" {
+			info := color.New(color.FgWhite, color.Bold).SprintFunc()
+			white := color.New(color.FgWhite, color.Bold).SprintFunc()
+			green := color.New(color.FgGreen, color.Bold).SprintFunc()
+
+			fullUrl := url
+			endPathLength := len(strings.Split(fullUrl, "/"))
+			endPath := strings.Split(fullUrl, "/")[endPathLength-1]
+			rootDomain := strings.Split(fullUrl, "/")[0] + "//" + strings.Split(fullUrl, "/")[2] + "/" + endPath
+
+			contentFound := testUrl + word
+			resp1, err := http.Get(contentFound)
+			if err != nil {
+				return
+			}
+
+			resp2, err := http.Get(rootDomain)
+			if err != nil {
+				return
+			}
+
+			if traverse {
+				if resp1.StatusCode == 200 && resp2.StatusCode != 200 {
+					if args.Output != "" {
+
+						f, err := os.Create(args.Output)
+						if err != nil {
+							return
+						}
+						defer f.Close()
+
+						_, err2 := f.WriteString(contentFound + "\n")
+
+						if err2 != nil {
+							return
+						}
+					}
+					fmt.Printf("\n\n%s%s%s %s\n\n", white("["), green("FOUND"), white("]"), info(contentFound))
+					defer color.Unset() // Use it in your function
+
+				}
+			} else {
+
+				if resp1.StatusCode == 200 {
+					if args.Output != "" {
+
+						f, err := os.Create(args.Output)
+						if err != nil {
+							return
+						}
+						defer f.Close()
+
+						_, err2 := f.WriteString(contentFound + "\n")
+
+						if err2 != nil {
+							return
+						}
+					}
+
+					fmt.Printf("\n\n%s%s%s %s\n\n", white("["), green("FOUND"), white("]"), info(contentFound))
+					defer color.Unset() // Use it in your function
+				}
 			}
 		} else {
+			info := color.New(color.FgWhite, color.Bold).SprintFunc()
+			white := color.New(color.FgWhite, color.Bold).SprintFunc()
+			green := color.New(color.FgGreen, color.Bold).SprintFunc()
 
-			if resp1.StatusCode == 200 {
-				if args.Output != "" {
+			contentNotFound := url + "/" + word
+			rootDomain := strings.Split(contentNotFound, "/")[0] + "//" + strings.Split(contentNotFound, "/")[2] + "/" + word
+			contentFound := testUrl + word
+			resp1, err := http.Get(contentFound)
+			if err != nil {
+				return
+			}
+			resp2, err := http.Get(rootDomain)
+			if err != nil {
+				return
+			}
 
-					f, err := os.Create(args.Output)
-					if err != nil {
-						return
+			if traverse {
+				if resp1.StatusCode == 200 && resp2.StatusCode != 200 {
+					if args.Output != "" {
+
+						f, err := os.Create(args.Output)
+						if err != nil {
+							return
+						}
+						defer f.Close()
+
+						_, err2 := f.WriteString(contentFound + "\n")
+
+						if err2 != nil {
+							return
+						}
 					}
-					defer f.Close()
+					fmt.Printf("\n\n%s%s%s %s\n\n", white("["), green("FOUND"), white("]"), info(contentFound))
+					defer color.Unset() // Use it in your function
 
-					_, err2 := f.WriteString(contentFound + "\n")
-
-					if err2 != nil {
-						return
-					}
 				}
+			} else {
 
-				fmt.Printf("\n\n%s%s%s %s\n\n", white("["), green("FOUND"), white("]"), info(contentFound))
-				defer color.Unset() // Use it in your function
+				if resp1.StatusCode == 200 {
+					if args.Output != "" {
+
+						f, err := os.Create(args.Output)
+						if err != nil {
+							return
+						}
+						defer f.Close()
+
+						_, err2 := f.WriteString(contentFound + "\n")
+
+						if err2 != nil {
+							return
+						}
+					}
+
+					fmt.Printf("\n\n%s%s%s %s\n\n", white("["), green("FOUND"), white("]"), info(contentFound))
+					defer color.Unset() // Use it in your function
+				}
 			}
 		}
-	}
 
+	}
 }
