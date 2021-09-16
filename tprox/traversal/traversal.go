eb3102b09d7863e290828dc9206c57207bb45446
zoidex <blake@pentesterlab.com>
2021-09-16 23:14:45 +1000

    Bug fixes

--- tprox/traversal/traversal.go
+++ tprox/traversal/traversal.go
@@ -41,175 +41,57 @@ func TestTraversal(wg *sync.WaitGroup, url string, payload string, silent bool, 
 
 	var testUrl = ""
 
-	if check {
-		discover.BruteForDirAndFile(*client, wg, url, url, "", silent, traverse, discoverContent, check)
-	} else {
-		if discoverContent {
-			// Start bruteforcing for files and directories
-			words := make(chan string)
+	if test {
+		// Get the test url
+		paths := strings.Split(url, "/")
+		pathCount := len(paths) - 2
+		testUrl = CraftTestUrl(pathCount, url, payload)
 
-			// Read in the wordlist list
-			wordFile, err := os.Open(args.Wordlist)
-			count := int64(len(LinesInFile(args.Wordlist)))
+		// Get the response from the server to
+		// Perform ongoing testing for proxy misconfigs
+		req, err := http.NewRequest("GET", testUrl, nil)
+		if err != nil {
+			return
+		}
+		resp, err := client.Do(req)
+		if err != nil {
+			return
+		}
+
+		// Check if the page is a 404, if it is we may be in the root api
+		// From here onwards we need to perform directory content discovery to
+		// Find internal files or endpoints
+		if resp.StatusCode == 400 {
+
+			// Get the test url
+			paths := strings.Split(url, "/")
+			pathCount := len(paths) - 3
+			testUrl = CraftTestUrl(pathCount, url, payload) + "doesnotexist"
+
+			// Get the response from the server to
+			// Perform ongoing testing for proxy misconfigs
+			req, err := http.NewRequest("GET", testUrl, nil)
 			if err != nil {
 				return
 			}
-			wordBytes := bufio.NewScanner(wordFile)
+			resp, err := client.Do(req)
 			if err != nil {
 				return
 			}
-			if progress {
-				bar := progressbar.Default(count)
-				for i := 0; i < args.Threads; i++ {
-					wg.Add(1)
-					go func() {
 
-						// wordlist brute channel loop
-						for word := range words {
-							discover.BruteForDirAndFile(*client, wg, url, url, word, silent, traverse, discoverContent, check)
-							bar.Add(1)
-						}
-						time.Sleep(40 * time.Millisecond)
-
-					}()
-					wg.Done()
-				}
-			} else {
-				for i := 0; i < args.Threads; i++ {
-					wg.Add(1)
-					go func() {
-
-						// wordlist brute channel loop
-						for word := range words {
-							discover.BruteForDirAndFile(*client, wg, url, url, word, silent, traverse, discoverContent, check)
-						}
-						time.Sleep(40 * time.Millisecond)
-
-					}()
-					wg.Done()
-				}
-			}
-
-			for wordBytes.Scan() {
-				words <- wordBytes.Text()
-			}
-			close(words)
-
-		} else {
-			if traverse {
-				// Get the test url
-				paths := strings.Split(url, "/")
-				pathCount := len(paths) - 2
-				testUrl = CraftTestUrl(pathCount, url, payload)
-
-				// Get the response from the server to
-				// Perform ongoing testing for proxy misconfigs
-				req, err := http.NewRequest("GET", testUrl, nil)
-				if err != nil {
-					return
-				}
-				resp, err := client.Do(req)
-				if err != nil {
-					return
-				}
-
-				// Check if the page is a 404, if it is we may be in the root api
-				// From here onwards we need to perform directory content discovery to
-				// Find internal files or endpoints
-				if resp.StatusCode == 400 {
-
-					// Get the test url
-					paths := strings.Split(url, "/")
-					pathCount := len(paths) - 3
-					testUrl = CraftTestUrl(pathCount, url, payload) + "doesnotexist"
-
-					// Get the response from the server to
-					// Perform ongoing testing for proxy misconfigs
-					req, err := http.NewRequest("GET", testUrl, nil)
-					if err != nil {
-						return
-					}
-					resp, err := client.Do(req)
-					if err != nil {
-						return
-					}
-
-					if resp.StatusCode == 404 {
-						color.NoColor = false
-						white := color.New(color.FgWhite, color.Bold).SprintFunc()
-						blue := color.New(color.FgBlue, color.Bold).SprintFunc()
-
-						fmt.Printf("\n%s%s%s %s\n\n", white("["), blue("PROXY FOUND"), white("]"), white(testUrl))
-						if args.Wordlist == "" {
-							return
-						}
-						if !test {
-							// Start bruteforcing for files and directories
-							words := make(chan string)
-
-							// Read in the wordlist list
-							wordFile, err := os.Open(args.Wordlist)
-							count := int64(len(LinesInFile(args.Wordlist)))
-							if err != nil {
-								return
-							}
-							wordBytes := bufio.NewScanner(wordFile)
-							if err != nil {
-								return
-							}
-							if progress {
-								bar := progressbar.Default(count)
-								for i := 0; i < args.Threads; i++ {
-									wg.Add(1)
-									go func() {
-
-										// wordlist brute channel loop
-										for word := range words {
-											discover.BruteForDirAndFile(*client, wg, url, CraftTestUrl(pathCount, url, payload), word, silent, traverse, discoverContent, check)
-											bar.Add(1)
-										}
-										time.Sleep(40 * time.Millisecond)
-
-									}()
-									wg.Done()
-								}
-
-							} else {
-
-								for i := 0; i < args.Threads; i++ {
-									wg.Add(1)
-									go func() {
-
-										// wordlist brute channel loop
-										for word := range words {
-											discover.BruteForDirAndFile(*client, wg, url, CraftTestUrl(pathCount, url, payload), word, silent, traverse, discoverContent, check)
-										}
-
-									}()
-									wg.Done()
-								}
-
-							}
-
-							for wordBytes.Scan() {
-								words <- wordBytes.Text()
-							}
-							close(words)
-						}
-
-					}
-				}
-			} else {
-
-				testUrl = url + "/"
+			if resp.StatusCode == 404 {
 				color.NoColor = false
 				white := color.New(color.FgWhite, color.Bold).SprintFunc()
 				blue := color.New(color.FgBlue, color.Bold).SprintFunc()
 
-				fmt.Printf("\n%s%s%s %s\n\n", white("["), blue("DISCOVERY"), white("]"), white(testUrl))
-				if args.Wordlist == "" {
-					return
-				}
+				fmt.Printf("\n%s%s%s %s\n\n", white("["), blue("PROXY FOUND"), white("]"), white(testUrl))
+			}
+		}
+	} else {
+		if check {
+			discover.BruteForDirAndFile(*client, wg, url, url, "", silent, traverse, discoverContent, check)
+		} else {
+			if discoverContent {
 				// Start bruteforcing for files and directories
 				words := make(chan string)
 
@@ -231,7 +113,7 @@ func TestTraversal(wg *sync.WaitGroup, url string, payload string, silent bool, 
 
 							// wordlist brute channel loop
 							for word := range words {
-								discover.BruteForDirAndFile(*client, wg, url, testUrl, word, silent, traverse, discoverContent, check)
+								discover.BruteForDirAndFile(*client, wg, url, url, word, silent, traverse, discoverContent, check)
 								bar.Add(1)
 							}
 							time.Sleep(40 * time.Millisecond)
@@ -239,26 +121,192 @@ func TestTraversal(wg *sync.WaitGroup, url string, payload string, silent bool, 
 						}()
 						wg.Done()
 					}
-
 				} else {
-
 					for i := 0; i < args.Threads; i++ {
 						wg.Add(1)
 						go func() {
 
 							// wordlist brute channel loop
 							for word := range words {
-								discover.BruteForDirAndFile(*client, wg, url, testUrl, word, silent, traverse, discoverContent, check)
+								discover.BruteForDirAndFile(*client, wg, url, url, word, silent, traverse, discoverContent, check)
 							}
-						}()
+							time.Sleep(40 * time.Millisecond)
 
+						}()
+						wg.Done()
 					}
-					wg.Done()
 				}
+
 				for wordBytes.Scan() {
 					words <- wordBytes.Text()
 				}
 				close(words)
+
+			} else {
+				if traverse {
+					// Get the test url
+					paths := strings.Split(url, "/")
+					pathCount := len(paths) - 2
+					testUrl = CraftTestUrl(pathCount, url, payload)
+
+					// Get the response from the server to
+					// Perform ongoing testing for proxy misconfigs
+					req, err := http.NewRequest("GET", testUrl, nil)
+					if err != nil {
+						return
+					}
+					resp, err := client.Do(req)
+					if err != nil {
+						return
+					}
+
+					// Check if the page is a 404, if it is we may be in the root api
+					// From here onwards we need to perform directory content discovery to
+					// Find internal files or endpoints
+					if resp.StatusCode == 400 {
+
+						// Get the test url
+						paths := strings.Split(url, "/")
+						pathCount := len(paths) - 3
+						testUrl = CraftTestUrl(pathCount, url, payload) + "doesnotexist"
+
+						// Get the response from the server to
+						// Perform ongoing testing for proxy misconfigs
+						req, err := http.NewRequest("GET", testUrl, nil)
+						if err != nil {
+							return
+						}
+						resp, err := client.Do(req)
+						if err != nil {
+							return
+						}
+
+						if resp.StatusCode == 404 {
+							color.NoColor = false
+							white := color.New(color.FgWhite, color.Bold).SprintFunc()
+							blue := color.New(color.FgBlue, color.Bold).SprintFunc()
+
+							fmt.Printf("\n%s%s%s %s\n\n", white("["), blue("PROXY FOUND"), white("]"), white(testUrl))
+							if args.Wordlist == "" {
+								return
+							}
+							if !test {
+								// Start bruteforcing for files and directories
+								words := make(chan string)
+
+								// Read in the wordlist list
+								wordFile, err := os.Open(args.Wordlist)
+								count := int64(len(LinesInFile(args.Wordlist)))
+								if err != nil {
+									return
+								}
+								wordBytes := bufio.NewScanner(wordFile)
+								if err != nil {
+									return
+								}
+								if progress {
+									bar := progressbar.Default(count)
+									for i := 0; i < args.Threads; i++ {
+										wg.Add(1)
+										go func() {
+
+											// wordlist brute channel loop
+											for word := range words {
+												discover.BruteForDirAndFile(*client, wg, url, CraftTestUrl(pathCount, url, payload), word, silent, traverse, discoverContent, check)
+												bar.Add(1)
+											}
+											time.Sleep(40 * time.Millisecond)
+
+										}()
+										wg.Done()
+									}
+
+								} else {
+
+									for i := 0; i < args.Threads; i++ {
+										wg.Add(1)
+										go func() {
+
+											// wordlist brute channel loop
+											for word := range words {
+												discover.BruteForDirAndFile(*client, wg, url, CraftTestUrl(pathCount, url, payload), word, silent, traverse, discoverContent, check)
+											}
+
+										}()
+										wg.Done()
+									}
+
+								}
+
+								for wordBytes.Scan() {
+									words <- wordBytes.Text()
+								}
+								close(words)
+							}
+
+						}
+					}
+				} else {
+
+					testUrl = url + "/"
+					color.NoColor = false
+					white := color.New(color.FgWhite, color.Bold).SprintFunc()
+					blue := color.New(color.FgBlue, color.Bold).SprintFunc()
+
+					fmt.Printf("\n%s%s%s %s\n\n", white("["), blue("DISCOVERY"), white("]"), white(testUrl))
+					if args.Wordlist == "" {
+						return
+					}
+					// Start bruteforcing for files and directories
+					words := make(chan string)
+
+					// Read in the wordlist list
+					wordFile, err := os.Open(args.Wordlist)
+					count := int64(len(LinesInFile(args.Wordlist)))
+					if err != nil {
+						return
+					}
+					wordBytes := bufio.NewScanner(wordFile)
+					if err != nil {
+						return
+					}
+					if progress {
+						bar := progressbar.Default(count)
+						for i := 0; i < args.Threads; i++ {
+							wg.Add(1)
+							go func() {
+
+								// wordlist brute channel loop
+								for word := range words {
+									discover.BruteForDirAndFile(*client, wg, url, testUrl, word, silent, traverse, discoverContent, check)
+									bar.Add(1)
+								}
+								time.Sleep(40 * time.Millisecond)
+
+							}()
+							wg.Done()
+						}
+
+					} else {
+
+						for i := 0; i < args.Threads; i++ {
+							wg.Add(1)
+							go func() {
+
+								// wordlist brute channel loop
+								for word := range words {
+									discover.BruteForDirAndFile(*client, wg, url, testUrl, word, silent, traverse, discoverContent, check)
+								}
+							}()
+
+						}
+						wg.Done()
+					}
+					for wordBytes.Scan() {
+						words <- wordBytes.Text()
+					}
+					close(words)
+				}
 			}
 		}
 	}
